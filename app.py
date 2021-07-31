3493dd6d47e6df5b8c867decb2298e3be7976ee7
Nazmu Masood <nazmumasood96@gmail.com>
2021-08-01 01:18:58 +0600

    check for empty survey page added

--- app.py
+++ app.py
@@ -15,12 +15,15 @@ from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from msedge.selenium_tools import EdgeOptions, Edge
 
-browser_type = 'chrome'
+print("\n\n------ Welcome to the EDU-Instructor-Survey-Automation-System ------\n")
+
+browser_type = ''
 while browser_type == '':
-    browser_type = input("Please enter browser name (chrome/firefox/edge): ")
+    browser_type = input("Please enter your preferred browser name (chrome/firefox/edge): ")
     if browser_type != 'chrome' and browser_type != 'firefox' and browser_type != 'edge':
         browser_type = ''
 
+print("Opening up "+browser_type+" browser...")
 if browser_type=='edge':
     options = EdgeOptions()
     # options.use_chromium = True
@@ -46,7 +49,8 @@ if browser_type=='chrome':
 #Loading the webpage
 driver.get('https://blackbox.eastdelta.edu.bd/einstein-freshair/index.jsp')
 root_window = driver.window_handles[0]
-print('Webpage title: '+driver.title)
+# print('\nWebpage title: '+driver.title)
+print('\n'+driver.title+" loaded")
 
 #Getting the login fields
 username = driver.find_element_by_name("username")
@@ -54,128 +58,148 @@ password = driver.find_element_by_name("password")
 loginButton = driver.find_element_by_id("loginBtn")
 driver.find_element_by_xpath("//select[@name='role']/option[text()='Student']").click()
 
-name= 'ayon3'
-pword= 'asd123123'
+name= ''
+pword= ''
+while name=='':
+    name = input("Enter Username: ").strip()
+while pword=='':
+    pword = input("Enter Password: ").strip()
 
+print("Attempting to login...")
 username.send_keys(name)
 password.send_keys(pword)
 loginButton.click()
 
 #---- Login Successful
-# instr_surv_table = driver.find_element_by_id('instructor_survey')
-# print(instr_surv_table.get_attribute('innerHTML'))
 
 try:
-    framesets = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.TAG_NAME, "frameset")))
+    framesets = WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located((By.TAG_NAME, "frameset")))
     # print(framesets[1].get_attribute("innerHTML"))
+    print("Login successful\n")
     driver.switch_to.frame("mainframe")
-    # print(driver.page_source)
     
-    # Navigating to the 'instructor_survey' section
-    instr_surv_table_id = "//table[@id='instructor_survey']"
-    instr_surv_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, instr_surv_table_id)))
-    print("'instructor_survey' table found!")
-    source_code = instr_surv_table.get_attribute("innerHTML")
-    # print(source_code)
+    try:
+        # Navigating to the 'instructor_survey' section
+        instr_surv_table_id = "//table[@id='instructor_survey']"
+        instr_surv_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, instr_surv_table_id)))
+        print("'instructor_survey' table found!")
+        source_code = instr_surv_table.get_attribute("innerHTML")
+        # print(source_code)
 
-    coursesHref = driver.find_elements_by_xpath("//*[@id='instructor_survey']/tbody/tr/td/a")
+        coursesHref = driver.find_elements_by_xpath("//*[@id='instructor_survey']/tbody/tr/td/a")
 
-    courses = []
-    courseLinks = [] 
-    for courseHref in coursesHref:
-        courseTitle = courseHref.get_attribute("innerHTML")
-        courses.append(courseTitle)
-        courseLink = courseHref.get_attribute("href")
-        courseLinks.append(courseLink)
+        courses = []
+        courseLinks = [] 
+        for courseHref in coursesHref:
+            courseTitle = courseHref.get_attribute("innerHTML")
+            courses.append(courseTitle)
+            courseLink = courseHref.get_attribute("href")
+            courseLinks.append(courseLink)
 
-    for courseTitle, courseLink in zip(courses, courseLinks):
-        driver.execute_script("window.open()")
-        time.sleep(1)
-        # Switch to the newly opened tab
-        driver.switch_to.window(driver.window_handles[1])
-        # Navigate to new URL in new tab
-        driver.get(courseLink)
-        print('Webpage title: '+driver.title)
+        for courseTitle, courseLink in zip(courses, courseLinks):
+            print("\nLoading "+courseTitle+" course survey page...")
+            driver.execute_script("window.open()")
+            time.sleep(1)
+            # Switch to the newly opened tab
+            driver.switch_to.window(driver.window_handles[1])
+            # Navigate to new URL in new tab
+            driver.get(courseLink)
+            # print('Webpage title: '+driver.title)
 
-        # print((driver.page_source))
-        spans = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "span")))
-        instructor = spans[1].text[19:len(spans[1].text)-1]
+            errorString = "Instructor evaluation survey for this class is over or not yet started."
+            if errorString in driver.page_source:
+                print(errorString)
+                driver.close()
+                driver.switch_to.window(root_window)
+                time.sleep(2)
+                continue
 
-        print("Course: "+courseTitle+" - Instructor: "+instructor)
-        print("Points criteria : \n 5) Strongly Agree \n 4) Agree \n 3) Neutral \n 2) Disagree \n 1) Strongly Disagree")
+            # print((driver.page_source))
+            spans = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "span")))
+            instructor = spans[1].text[19:len(spans[1].text)-1]
 
-        def representsInt(s):
-            try: 
-                int(s)
-                return True
-            except ValueError:
-                return False
+            print("Course: "+courseTitle+" - Instructor: "+instructor)
+            print("Points criteria : \n 5) Strongly Agree \n 4) Agree \n 3) Neutral \n 2) Disagree \n 1) Strongly Disagree")
 
-        reviewPoint = 0
-        while (reviewPoint==0):
-            reviewPoint = input("What points do you want to give (1-5): ")
-            if representsInt(reviewPoint):
-                reviewPoint = int(reviewPoint)
-                if not (1<=reviewPoint<=5):
+            def representsInt(s):
+                try: 
+                    int(s)
+                    return True
+                except ValueError:
+                    return False
+
+            reviewPoint = 0
+            while (reviewPoint==0):
+                reviewPoint = input("What points do you want to give (1-5): ")
+                if representsInt(reviewPoint):
+                    reviewPoint = int(reviewPoint)
+                    if not (1<=reviewPoint<=5):
+                        reviewPoint = 0
+                else:
                     reviewPoint = 0
-            else:
-                reviewPoint = 0
-        
-        # print("Your selected point: "+str(reviewPoint))
+            
+            # print("Your selected point: "+str(reviewPoint))
 
-        comment = ''
-        while comment=='':
-            comment = input("Comment and suggestions: ").strip()
+            comment = ''
+            while comment=='':
+                comment = input("Comment and suggestions: ").strip()
 
 
-        # -------- Page 1
-        radios = []
-        nextButton = ''
-        inputs = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "input")))
-        for input in inputs:
-            if input.get_attribute("type") == 'radio':
-                # print(input.get_attribute("onclick"))
-                radios.append(input)
-            if input.get_attribute("name") == 'Next':
-                nextButton = input
-                # print("next button found")
+            # -------- Page 1
+            radios = []
+            nextButton = ''
+            inputs = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "input")))
+            for input in inputs:
+                if input.get_attribute("type") == 'radio':
+                    # print(input.get_attribute("onclick"))
+                    radios.append(input)
+                if input.get_attribute("name") == 'Next':
+                    nextButton = input
+                    # print("next button found")
 
-        for radio in radios:
-            onclick = radio.get_attribute("onclick")
-            optionNo = int(onclick[len(onclick)-3:len(onclick)-2])
-            if optionNo == reviewPoint:
-                radio.click()
+            print("Filling up details on page 1...")
+            for radio in radios:
+                onclick = radio.get_attribute("onclick")
+                optionNo = int(onclick[len(onclick)-3:len(onclick)-2])
+                if optionNo == reviewPoint:
+                    radio.click()
 
-        nextButton.click()
+            nextButton.click()
 
 
-        # -------- Page 2
-        radios = []
-        finishButton = ''
-        inputs = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "input")))
-        for input in inputs:
-            if input.get_attribute("type") == 'radio':
-                # print(input.get_attribute("onclick"))
-                radios.append(input)
-            if input.get_attribute("name") == 'Finish':
-                finishButton = input
-                print("finish button found")
+            # -------- Page 2
+            radios = []
+            finishButton = ''
+            inputs = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "input")))
+            for input in inputs:
+                if input.get_attribute("type") == 'radio':
+                    # print(input.get_attribute("onclick"))
+                    radios.append(input)
+                if input.get_attribute("name") == 'Finish':
+                    finishButton = input
+                    print("finish button found")
 
-        for radio in radios:
-            onclick = radio.get_attribute("onclick")
-            optionNo = int(onclick[len(onclick)-3:len(onclick)-2])
-            if optionNo == reviewPoint:
-                radio.click()
+            print("Filling up details on page 2...")
+            for radio in radios:
+                onclick = radio.get_attribute("onclick")
+                optionNo = int(onclick[len(onclick)-3:len(onclick)-2])
+                if optionNo == reviewPoint:
+                    radio.click()
 
-        commentBox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "textarea")))
-        commentBox.send_keys(comment)
+            commentBox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "textarea")))
+            commentBox.send_keys(comment)
 
-        # finishButton.click()
+            # finishButton.click()
 
-        time.sleep(2)
-        driver.switch_to.window(root_window)
-        # break
+            time.sleep(2)
+            # driver.close()
+            driver.switch_to.window(root_window)
+            # break
 
+    except TimeoutException:
+        print("No 'instructor_survey' table found")
 except TimeoutException:
-    print("No 'instructor_survey' table found")
+    print("Login failed. Please try again.")
+
+print("\nProgram finished")
 
